616b05f8059c956ce5630b16e9b9ab4336e48a82
fscopulovic-tgm <fscopulovic@student.tgm.ac.at>
2016-04-08 10:17:29 +0200

    initial commit

---
+++ src/StringTokenizerTest.java
@@ -0,0 +1,17 @@
+/*
+ * @author: Filip Scopulovic
+ * @date: 08.04.2016
+ * @use: StringTokenizer test cases
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringTokenizerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
